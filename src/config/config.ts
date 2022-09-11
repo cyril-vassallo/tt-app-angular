848fbe0655ffe9671b94acd9291b33243d5602b6
CYRIL VASSALLO <42696124+cyril-vassallo@users.noreply.github.com>
2022-09-11 23:39:32 +0200

    update angular app config

--- src/config/config.ts
+++ src/config/config.ts
@@ -17,5 +17,5 @@ export const constant = {
 }
 
 if (environment.production) {
-    constant.API_URL =  'https://api-taskiteam.herokuapp.com/'
+    constant.API_URL =  'https://api-taskiteam.herokuapp.com'
 }
